b9ab099387d847f7456da22a9f9a32075fe4e4bd
RaswanthGS <raswanth47@gmail.com>
2022-08-10 15:51:10 +0530

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%1ius_xldgdz#%#)=62$#3*21iogrom(pcet&(o5zw*++ynd!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cscans-demo.herokuapp.com','127.0.0.1']
 
 
 # Application definition
